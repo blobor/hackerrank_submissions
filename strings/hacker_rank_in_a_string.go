ffec4a7b943d7adbcc5dd50487bfb7e618720c7e
Bohdan Lobor <b.lobor@outlook.com>
2019-12-13 19:18:27 +0200

    refactor(HackerrankInString): comment and function name should be same

--- strings/hacker_rank_in_a_string.go
+++ strings/hacker_rank_in_a_string.go
@@ -3,7 +3,7 @@ package strings
 // Strings: HackerRank in a String!
 // More info: https://www.hackerrank.com/challenges/hackerrank-in-a-string
 
-// HackerRankInString must return a string YES or NO depending if input string contains
+// HackerrankInString must return a string YES or NO depending if input string contains
 // the word hackerrank if a subsequence of its characters spell the word hackerrank.
 func HackerrankInString(s string) string {
 	q := []rune("hackerrank")
